77b347deaf34f82278f9e81f426ab8a503bb88ef
Livingston-k <kaddulivingston@gmail.com>
2022-09-29 08:44:09 +0300

    loading screen on search

--- src/PokeLoader.js
+++ src/PokeLoader.js
@@ -1,14 +1,23 @@
 import React from "react"
-import {View,Text} from "react-native"
+import {View,StyleSheet,Imager} from "react-native"
 
+var backGround = require('../assets/icons/loader.gif')
 class PokeLoader extends React.Component{
     render(){
         return (
             <View>
-                <Text>We are loading</Text>
+               <Image style={styles.img} source={backGround}/>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+  img: {
+   width:400,
+   height:400,
+   justifyContent:'center',
+   alignItems:'center'
+  },
+});
 
 export default PokeLoader;
